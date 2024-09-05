9c5a24d15e5e98f3c4e462c7401999f12d74246f
OptimalAches <sachanakshat09@gmail.com>
2024-09-05 22:51:50 +0530

    made ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,11 @@ import Footer from './components/Footer'
 function App() {
 
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className='min-h-screen flex flex-col bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
       <Navbar />
 
-      <div className="bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-        <Manager />
-      </div>
-
+      <Manager />
+    
       <Footer />
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='bg-slate-800 text-white '>
+        <nav className='bg-slate-800 text-white'>
             <div className='py-2 px-4 md:mycontainer flex justify-between items-center'>
                 <div className="logo font-bold text-white text-2xl">
                     <span className='text-green-500'>&lt;</span><span>Cipher</span><span className="text-green-500">SAFE/&gt;</span>
